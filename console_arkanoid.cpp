a5761f6da57bb38e4df307da8fa6e882fe17a0cb
KarolPuczynski <karol.puczynski123@gmail.com>
2025-06-12 01:15:30 +0200

    Add files via upload

---
+++ console_arkanoid.cpp
@@ -0,0 +1,259 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <queue>
+
+using namespace std;
+
+struct game_board {
+    int rows, columns, r, n, blocks, rnd;
+    int x_paddle, y_paddle;
+    vector<vector<int>> board;
+    vector<queue<int>> blocks_queue;
+};
+
+game_board initialization(int columns, int r, int n, int blocks) {
+    game_board state = { .rows = blocks + 5, .columns = columns, .r = r, .n = n, .blocks = blocks };
+    for (int i = 0; i < state.columns; i++) {
+        state.blocks_queue.push_back(queue<int>());
+    }
+    state.y_paddle = state.rows - 1;
+    state.x_paddle = state.columns / 2;
+    if (state.n)
+        state.rnd = rand() % n;
+    else
+        state.rnd = 0;
+    state.board.resize(state.rows, vector<int>(columns, 0));
+    return state;
+}
+
+void menu();
+void creating_board(game_board& state);
+void display_board(game_board& state);
+void moving_left(game_board& state);
+void moving_right(game_board& state);
+void shooting(game_board& state, int& moves_left, int& score);
+void extra_block(game_board& state);
+void game_ending(game_board& state);
+
+int main()
+{
+    menu();
+    return 0;
+}
+
+void menu() {
+    int columns, r, n, blocks;
+    srand(time(NULL));
+    system("cls");
+    cout << "Witaj w grze arkanoid!" << endl;
+    cout << endl << "Podaj ilosc kolumn planszy: ";
+    cin >> columns;
+    cout << endl << "Podaj ilosc bloczkow znajdujacych sie w kazdej kolumnie: ";
+    cin >> blocks;
+    cout << endl << "Co r+rnd ruchow do wszystkich kolumn dodawany jest jeden bloczek zmniejszajac przestrzen dla gracza. Parametr rnd jest wartoscia losowa z przedzialu[0, n)." << endl;
+    cout << endl << "Podaj parametr r: ";
+    cin >> r;
+    cout << endl << "Podaj parametr n: ";
+    cin >> n;
+
+
+    game_board state = initialization(columns, r, n, blocks);
+    creating_board(state);
+}
+
+void creating_board(game_board& state) {
+
+    int propability_of_bonus = 20;
+    for (int i = 0; i < state.rows; i++) {
+        for (int j = 0; j < state.columns; j++) {
+            state.board[i][j] = 0;
+        }
+    }
+
+    for (int i = state.blocks - 1; i >= 0; i--) { //filling blocks in
+        for (int j = state.columns - 1; j >= 0; j--) {
+            int rand_number = rand() % 100 + 1;
+            if (rand_number > propability_of_bonus || i == 0 || i == state.blocks - 1) {
+                state.blocks_queue[j].push(1);
+                state.board[i][j] = 1;
+            }
+            else {
+                int rand_bonus = rand() % 2 + 2;
+                state.blocks_queue[j].push(rand_bonus); // bonus 2: '::', bonus 3: '++'
+                state.board[i][j] = rand_bonus;
+            }
+        }
+    }
+    state.board[state.y_paddle][state.x_paddle] = 4; // paddle index
+
+    display_board(state);
+
+}
+
+
+void display_board(game_board& state) {
+    char move;
+    int moves_left = state.r + state.rnd;
+    int score = 0;
+    while (true) {
+        system("cls");
+        cout << "                                   A R K A N O I D\n\n\n";
+
+        cout << "            -------------------------------------------------------\n";
+        cout << "            |     S T E R O W A N I E    |       B O N U S Y      |\n";
+        cout << "            |                            |                        |\n";
+        cout << "            |      A - ruch w lewo       |     #::# - zbicie      |\n";
+        cout << "            |      D - ruch w prawo      |     dwoch bloczkow     |\n";
+        cout << "            |      W - strzal            |     #++# - dodatkowe   |\n";
+        cout << "            |      Q - wyjscie z gry     |       5 ruchow         |\n";
+        cout << "            |                            |                        |\n";
+        cout << "            -------------------------------------------------------\n";
+        cout << "                                     WYNIK: " << score << "                    \n";
+        cout << "            -------------------------------------------------------\n\n";
+
+
+        cout << "       ";
+        for (int i = 0; i < (5 * state.columns - 1); i++) { //creating borders
+            cout << '-';
+        }
+        cout << '\n';
+
+        for (int i = 0; i < state.rows; i++) {
+            cout << "       ";
+            for (int j = 0; j < state.columns; j++) {
+                if (i <= state.blocks_queue[j].size() - 1 && state.blocks_queue[j].size() > 0) {
+                    switch (state.board[i][j]) {
+                    case 1:
+                        cout << "#### ";
+                        break;
+                    case 2: // bonus 2: '::'
+                        cout << "#::# ";
+                        break;
+                    case 3: // bonus 2: '++'
+                        cout << "#++# ";
+                        break;
+                    }
+                }
+                else if (state.board[i][j] == 4) {
+                    cout << "=[]= ";
+                }
+                else {
+                    cout << "     ";
+                }
+            }
+            if (i == 0) {
+                cout << "              Za " << moves_left << " ruchow pojawi sie";
+            }
+            else if (i == 1) {
+                cout << "               " << " dodatkowy bloczek   ";
+            }
+            cout << endl;
+        }
+        cout << "       ";
+        for (int i = 0; i < (5 * state.columns - 1); i++) { //creating borders
+            cout << '-';
+        }
+
+        game_ending(state);
+
+        cout << "\n\nWykonaj ruch: ";
+        cin >> move;
+
+        switch (move) {
+        case 'A': moving_left(state); break;
+        case 'D': moving_right(state); break;
+        case 'W': shooting(state, moves_left, score); break;
+        case 'Q': menu(); break;
+        }
+        moves_left--;
+        if (!moves_left) {
+            extra_block(state);
+            moves_left = state.r + state.rnd;
+        }
+    }
+}
+
+void moving_left(game_board& state) {
+    if (state.x_paddle > 0) {
+        state.board[state.y_paddle][state.x_paddle] = 0;
+        state.x_paddle--;
+        state.board[state.y_paddle][state.x_paddle] = 4;
+    }
+}
+
+void moving_right(game_board& state) {
+    if (state.x_paddle < state.columns - 1) {
+        state.board[state.y_paddle][state.x_paddle] = 0;
+        state.x_paddle++;
+        state.board[state.y_paddle][state.x_paddle] = 4;
+    }
+}
+
+void shooting(game_board& state, int& moves_left, int& score) {
+    if (!state.blocks_queue[state.x_paddle].empty()) {
+        int queue_size = state.blocks_queue[state.x_paddle].size();
+        int newest_element = state.blocks_queue[state.x_paddle].front();
+        switch (newest_element) {
+        case 1: //simple shot
+            state.blocks_queue[state.x_paddle].pop();
+            state.board[queue_size - 1][state.x_paddle] = 0;
+            score++;
+            break;
+        case 2: // bonus 2: '::' 
+            state.blocks_queue[state.x_paddle].pop();
+            state.board[queue_size - 1][state.x_paddle] = 0;
+            if (!state.blocks_queue[state.x_paddle].empty()) {
+                state.board[queue_size - 2][state.x_paddle] = 0;
+                shooting(state, moves_left, score);
+            }
+            score += 2;
+            break;
+        case 3: // bonus 3: '++'
+            moves_left += 5;
+            state.blocks_queue[state.x_paddle].pop();
+            state.board[queue_size - 1][state.x_paddle] = 0;
+            score += 2;
+            break;
+        }
+    }
+}
+
+void extra_block(game_board& state) {
+    int rand_column = rand() % state.columns;
+    state.blocks_queue[rand_column].push(1);
+    int size_queue = state.blocks_queue[rand_column].size();
+    state.board[size_queue - 1][rand_column] = 1;
+}
+
+void game_ending(game_board& state) {
+    char input;
+    int lost = 0;
+    int win = 0;
+    for (int i = 0; i < state.columns; i++) {
+        if (state.blocks_queue[i].size() >= state.rows) {
+            lost = 1;
+        }
+        if (!state.blocks_queue[i].empty()) {
+            win = 1;
+        }
+    }
+    if (lost == 1) {
+        cout << "\n\nP R Z E G R A L E S\n\n";
+        cout << "Czy chcesz sprobwac jeszcze raz? (T - tak, N - nie) ";
+        cin >> input;
+        switch (input) {
+        case 'T': menu(); break;
+        case 'N': exit(0); break;
+        }
+    }
+    if (win == 0) {
+        cout << "\n\nW Y G R A L E S\n\n";
+        cout << "Czy chcesz sprobwac jeszcze raz? (T - tak, N - nie)";
+        cin >> input;
+        switch (input) {
+        case 'T': menu(); break;
+        case 'N': exit(0); break;
+        }
+    }
+}
---
+++ waterworks.cpp
@@ -0,0 +1,349 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void creating_city(int rows, int columns);
+void menu();
+int checking_number();
+bool valid_city(char** city, int rows, int columns);
+void BFS(bool* visited, int** adj_list, int** waterWorks_list, int number_of_cities);
+void swapping(int* city_queue, int number_of_cities);
+void generating_waterworks(int** waterWorks_list, char** city, int rows, int columns);
+void drawing_city(char** city, int rows, int columns);
+void drawing_city_with_waterworks(char** city, int rows, int columns);
+
+int main()
+{
+	srand(time(0));
+	cout << "             Program autorstwa Karola Puczynskiego i Piotra Rokity" << endl;
+	menu();
+
+	return 0;
+}
+
+void menu() {
+	char input;
+	int rows = 0, columns = 0;
+	do {
+		cout << endl << "Czy chcesz wygenerowac mape miasta?(T - Tak, N - Nie) ";
+		cin >> input;
+		if (input == 'T') {
+			cout << endl << "Podaj wysokosc mapy miasta: ";
+			rows = checking_number();
+			cout << endl << "Podaj szerokosc mapy miasta: ";
+			columns = checking_number();
+			creating_city((2 * rows) - 1, (2 * columns) - 1);
+		}
+		else if (input == 'N') {
+			break;
+		}
+	} while (input != 'T' && input != 'N');
+}
+
+int checking_number() { // sprawdzanie czy jest wpisywana liczba
+	int input;
+	while (!(cin >> input)) {
+		cout << "Bledne dane. Wprowadz liczbe: ";
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+	return input;
+}
+
+void creating_city(int rows, int columns) {
+	int curr_letter = 65;
+	int probability = 40;
+	char** city = new char* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		city[i] = new char[columns];
+	}
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			city[i][j] = ' ';
+		}
+	}
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			if (curr_letter == 121) {
+				curr_letter = 65;
+			}
+
+			int random_number = rand() % 100;
+			if (j % 2 == 0 && i % 2 == 0) {
+				city[i][j] = char(curr_letter);
+				curr_letter++;
+			}
+			else if (j % 2 == 0 && i % 2 == 1) {
+				if (probability > random_number) {
+					city[i][j] = '|';
+				}
+				else {
+					city[i][j] = ' ';
+				}
+			}
+			else if (j % 2 == 1 && i % 2 == 0) {
+				if (probability > random_number) {
+					city[i][j] = '-';
+				}
+				else {
+					city[i][j] = ' ';
+				}
+			}
+		}
+	}
+
+	if (!valid_city(city, rows, columns)) {
+		creating_city(rows, columns);
+	}
+
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] city[i];
+	}
+	delete[] city;
+	menu();
+}
+
+bool valid_city(char** city, int rows, int columns) {
+
+	char start = city[0][0];
+	int number_of_cities = ((rows + 1) / 2) * ((columns + 1) / 2);
+
+	int** adj_list = new int* [number_of_cities];
+	int** waterWorks_list = new int* [number_of_cities];
+	bool* visited = new  bool[number_of_cities];
+
+	for (int i = 0; i < number_of_cities; i++) {
+		adj_list[i] = new int[number_of_cities];
+		waterWorks_list[i] = new int[number_of_cities];
+		visited[i] = false;
+	}
+	visited[0] = true;
+
+	for (int i = 0; i < number_of_cities; i++) {
+		for (int j = 0; j < number_of_cities; j++) {
+			adj_list[i][j] = 0;
+			waterWorks_list[i][j] = 0;
+		}
+	}
+
+	int neighbor_index = 0;
+	int city_index = 0;
+	int acctual_city = 0;
+	int acctual_columns = (columns + 1) / 2;
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+
+
+			if (i % 2 == 0 && j % 2 == 0)
+			{
+				acctual_city = j / 2 + (i / 2 * acctual_columns);
+
+				if (i + 1 < rows && city[i + 1][j] == '|') // pod
+				{
+					neighbor_index = acctual_city + acctual_columns;
+					adj_list[city_index][neighbor_index] = 1;
+				}
+				if (i - 1 > 0 && city[i - 1][j] == '|') // nad
+				{
+					neighbor_index = acctual_city - acctual_columns;
+					adj_list[city_index][neighbor_index] = 1;
+				}
+				if (j + 1 < columns && city[i][j + 1] == '-') // prawo
+				{
+					neighbor_index = acctual_city + 1;
+					adj_list[city_index][neighbor_index] = 1;
+				}
+				if (j - 1 > 0 && city[i][j - 1] == '-') // lewo
+				{
+					neighbor_index = acctual_city - 1;
+					adj_list[city_index][neighbor_index] = 1;
+				}
+				city_index++;
+			}
+		}
+	}
+
+
+	BFS(visited, adj_list, waterWorks_list, number_of_cities);
+
+	for (int i = 0; i < number_of_cities; i++) {
+		if (!visited[i]) {
+			for (int j = 0; j < rows; j++)
+			{
+				delete[] city[j];
+			}
+			delete[] city;
+			return false;
+		}
+	}
+
+
+	drawing_city(city, rows, columns);
+
+	generating_waterworks(waterWorks_list, city, rows, columns);
+
+	for (int i = 0; i < number_of_cities; i++)
+	{
+		delete[] waterWorks_list[i];
+		delete[] adj_list[i];
+	}
+	delete[] waterWorks_list;
+	delete[] adj_list;
+	delete[] visited;
+
+	return true;
+}
+
+void BFS(bool* visited, int** adj_list, int** waterWorks_list, int number_of_cities) {
+	int* city_queue = new int[number_of_cities];
+	for (int i = 0; i < number_of_cities; i++) {
+		city_queue[i] = -1;
+	}
+
+	city_queue[0] = 0;
+	int available_index = 0;
+
+	while (city_queue[0] != -1) {
+		int currVertex = city_queue[0];
+		city_queue[0] = -1;
+		if (available_index != 0) {
+			available_index--;
+			swapping(city_queue, number_of_cities);
+		}
+
+		for (int i = 0; i < number_of_cities; i++) {
+			if (adj_list[currVertex][i] == 1 && !visited[i]) {
+				visited[i] = true;
+				waterWorks_list[currVertex][i] = 1;
+				city_queue[available_index] = i;
+				available_index++;
+			}
+		}
+	}
+	delete[] city_queue;
+}
+
+void swapping(int* city_queue, int number_of_cities) {
+	for (int i = 0; i < number_of_cities - 1; i++) {
+		city_queue[i] = city_queue[i + 1];
+		city_queue[i + 1] = -1;
+	}
+}
+
+void generating_waterworks(int** waterWorks_list, char** city, int rows, int columns) {
+	int number_of_cities = ((rows + 1) / 2) * ((columns + 1) / 2);
+	int city_index = 0;
+	int neighbor_index = 0;
+	int acctual_city = 0;
+	int acctual_colums = (columns + 1) / 2;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			if (i % 2 == 0 && j % 2 == 0)
+			{
+				acctual_city = j / 2 + (i / 2 * acctual_colums);
+
+				if (i + 1 < rows && city[i + 1][j] == '|')
+				{
+					neighbor_index = acctual_city + acctual_colums;
+					if (waterWorks_list[city_index][neighbor_index] == 1)
+						city[i + 1][j] = 'i';
+				}
+				if (i - 1 > 0 && city[i - 1][j] == '|')
+				{
+					neighbor_index = acctual_city - acctual_colums;
+					if (waterWorks_list[city_index][neighbor_index] == 1)
+						city[i - 1][j] = 'i';
+				}
+				if (j + 1 < columns && city[i][j + 1] == '-')
+				{
+					neighbor_index = acctual_city + 1;
+					if (waterWorks_list[city_index][neighbor_index] == 1)
+						city[i][j + 1] = '=';
+				}
+				if (j - 1 > 0 && city[i][j - 1] == '-')
+				{
+					neighbor_index = acctual_city - 1;
+					if (waterWorks_list[city_index][neighbor_index] == 1)
+						city[i][j - 1] = '=';
+				}
+				city_index++;
+			}
+		}
+	}
+
+	drawing_city_with_waterworks(city, rows, columns);
+}
+
+void drawing_city(char** city, int rows, int columns) {
+	cout << endl << "City: " << endl;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			if (city[i][j] == '-') {
+				cout << "----";
+			}
+			else if (city[i][j] == '|') {
+				cout << "| ";
+			}
+			else if (city[i][j] == ' ') {
+				if (i % 2 == 0) {
+					cout << "    ";
+				}
+				else if (j % 2 == 1) {
+					cout << "   ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			else {
+				cout << city[i][j];
+			}
+		}
+		cout << endl;
+	}
+}
+
+void drawing_city_with_waterworks(char** city, int rows, int columns) {
+	cout << endl << "Waterworks: " << endl;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			if (city[i][j] == '=') {
+				cout << "====";
+			}
+			else if (city[i][j] == '-') {
+				cout << "----";
+			}
+			else if (city[i][j] == '|') {
+				cout << "| ";
+			}
+			else if (city[i][j] == 'i' && i % 2 == 1) {
+				cout << "||";
+			}
+			else if (city[i][j] == ' ') {
+				if (i % 2 == 0) {
+					cout << "    ";
+				}
+				else if (j % 2 == 1) {
+					cout << "   ";
+				}
+				else {
+					cout << "  ";
+				}
+			}
+			else {
+				cout << city[i][j];
+			}
+		}
+		cout << endl;
+	}
+}
